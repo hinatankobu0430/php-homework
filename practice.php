5ac231321fdc2e519f7c1f7b2b605d09fc6bde86
HinataOe <hinatan430_6521@yahoo.co.jp>
2021-01-20 03:12:26 +0000

    php/laravel_homework01

---
+++ practice.php
@@ -0,0 +1,46 @@
+//1.
+<?php
+$a=3;
+$b=7;
+echo $a+$b;
+?>
+
+//2.
+<?php
+$array_month = [
+    "jan" => "1月",
+    "feb" => "2月",
+    "mar" => "3月",
+    "apr" => "4月",
+    "may" => "5月",
+    "jun" => "6月",
+    "jul" => "7月",
+    "aug" => "8月",
+    "sep" => "9月",
+    "oct" => "10月",
+    "nov" => "11月",
+    "dec" => "12月"
+    ];
+    echo $array_month["aug"];
+    ?>
+
+//3.
+<?php
+$hello = "Hello";
+$name = " Hinata";
+$world = "'s World";
+echo $hello.$name.$world;
+?>
+
+//4.
+<?php
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+?>
+
+//5.
+$2018_calender =>　変数の1文字目に数字は×　この変数を使いたいのであれば"$calender_2018"
+"{}" => {}は連想配列では×　正しくは　"[]"を使う
+"February" => "2月"、 =>" 、"は×　正しくは","
+ "October" => 10月, => ""で囲っていないため×　正しくは　"10月"
